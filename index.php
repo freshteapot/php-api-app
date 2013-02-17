1bb17f638075b1c85099e742d98e4b47df4d5341
freshteapot <chris@freshteapot.net>
2013-02-17 17:49:28 +0000

    Removed some controllers. Will create an example folder with how to use it.

--- index.php
+++ index.php
@@ -2,5 +2,23 @@
 define ("APP_DIRECTORY", __DIR__ . DIRECTORY_SEPARATOR . 'src' );
 include APP_DIRECTORY . "/Freshteapot/bootstrap.php";
 
-$a = new \Freshteapot\Api\Server( "get", "/document", array() );
+
+$method = "put";
+$uri = "/list/123/changeorder";
+
+$config = array(
+    "pathToRoutes" => APP_DIRECTORY . "/routes.config"
+);
+
+$router = new \Freshteapot\Api\Router();
+$router->addRoutes($router->loadConfig($config['pathToRoutes']));
+$headers = array("Cache-control none;");
+
+
+$extra = array(
+    "internal" => true,
+    "command-line" => true
+);
+
+$a = new \Freshteapot\Api\ServerRoute( $method, $uri, $headers, $router );
 echo $a;
